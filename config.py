7ab0276a5005f1adee3093a978f0734c5f9a914e
iassert <dimakuzn42@gmail.com>
2024-03-03 12:19:46 +0200

    save

--- config.py
+++ config.py
@@ -2,20 +2,20 @@ from aiogram.bot.api import TelegramAPIServer
 
 
 class Config:
-    TOKEN: str
-    CREATOR_ID: int
-    OWNERS_ID: list[int]  = [CREATOR_ID]
+    TOKEN: str = "6288904451:AAGzx3XAviy_AjKNVQ71WPR1UKJD2pzgzTw"
+    CREATOR_ID: int = 704750195
+    OWNERS_ID: list[int]  = [CREATOR_ID, 364156591]
 
-    MAIN_GROUP_ID: int
+    MAIN_GROUP_ID: int = -1001780886382
 
-    LOCAL_SERVER:TelegramAPIServer  = TelegramAPIServer.from_base('http://localhost:8081')
+    LOCAL_SERVER: TelegramAPIServer = TelegramAPIServer.from_base('http://localhost:8081')
 
     DATABASE: str = "gpt"
     USER:     str = "assert"
     PASSWORD: str = "KhERp2ni_ALpRMdv3paBJUG-hVOryTu0"
-    HOST:     str
+    HOST:     str = "172.17.128.1"
     PORT:     str = "5434"
 
-    API_KEY: str
+    API_KEY: str = "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"
 
-    СHAT_GPT_BOT_NAME: str
+    СHAT_GPT_BOT_NAME: str = "Влад"
